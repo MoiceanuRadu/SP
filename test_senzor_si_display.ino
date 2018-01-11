e001d6c60b9712dec0d8344f0701a612078568bd
MoiceanuRadu <32989435+MoiceanuRadu@users.noreply.github.com>
2018-01-11 17:59:50 +0200

    Add files via upload

---
+++ test_senzor_si_display.ino
@@ -0,0 +1,37 @@
+#include <LiquidCrystal.h>
+
+
+LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
+int sensorPin = A1;
+int sensorValue = 0;
+
+void setup() {
+pinMode(sensorPin, INPUT);
+lcd.begin(16, 2);
+lcd.print("Cal.aer:");
+lcd.setCursor(0, 1);
+lcd.print("Indice:");
+}
+
+void loop() {
+lcd.setCursor(15, 1);
+lcd.print(" ");
+delay(1000);
+sensorValue = analogRead(sensorPin);
+lcd.setCursor(8, 1);
+lcd.print("     ");
+lcd.setCursor(8, 1);
+lcd.print(sensorValue);
+lcd.setCursor(9, 0);
+
+if (sensorValue < 40) lcd.print("F.Buna ");
+if ((sensorValue >= 40) and (sensorValue < 70)) lcd.print("Buna   ");
+if ((sensorValue >= 70) and (sensorValue < 100)) lcd.print("Medie  ");
+if ((sensorValue >= 100) and (sensorValue < 300)) lcd.print("Viciat ");
+if ((sensorValue >= 300) and (sensorValue < 500)) lcd.print("Toxic  ");
+if (sensorValue >= 500) lcd.print("Pericol");
+
+lcd.setCursor(15, 1);
+lcd.print("*");
+delay(1000);
+}
